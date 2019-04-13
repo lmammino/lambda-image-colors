040cc8f8d82ad4b074614ada676df14e891a6695
Luciano <lucianomammino@gmail.com>
2019-04-13 20:05:20 +0100

    Converted base code into lambda

--- main.go
+++ main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 
-	"github.com/EdlinOrg/prominentcolor"
-	"image"
-	_ "image/jpeg"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/lmammino/lambda-image-colors/utils"
 
-	"loige.co/image-colors/utils"
+	elasticsearch "github.com/elastic/go-elasticsearch/v7"
+	"github.com/elastic/go-elasticsearch/v7/esapi"
 )
 
-func main() {
+type ImageRecord struct {
+	ID     string   `json:"-"`
+	Key    string   `json:"key"`
+	Bucket string   `json:"bucket"`
+	Colors []string `json:"colors"`
+}
+
+func HandleRequest(ctx context.Context, event events.S3Event) error {
 	palette := utils.Palette{
 		"red":       {255, 0, 0},
 		"orange":    {255, 165, 0},
@@ -27,18 +41,80 @@ func main() {
 		"white":     {255, 255, 255},
 	}
 
-	fmt.Println(palette)
+	awsSession := session.New()
+	s3Client := s3.New(awsSession)
 
-	fmt.Println(os.Args[1])
-
-	f, _ := os.Open(os.Args[1])
-	defer f.Close()
-
-	img, _, _ := image.Decode(f)
-	res, _ := prominentcolor.Kmeans(img)
-
-	fmt.Println(res)
-	for _, color := range res {
-		fmt.Println(utils.GetClosestColor(color.Color.R, color.Color.G, color.Color.B, palette))
+	esHosts := os.Getenv("ES_HOSTS")
+	if esHosts == "" {
+		esHosts = "http://localhost:9200"
 	}
+	esIndex := os.Getenv("ES_INDEX")
+	if esIndex == "" {
+		esIndex = "images"
+	}
+
+	cfg := elasticsearch.Config{
+		Addresses: strings.Split(esHosts, ","),
+	}
+	esClient, err := elasticsearch.NewClient(cfg)
+	if err != nil {
+		return err
+	}
+
+	for _, s3record := range event.Records {
+		bucket := s3record.S3.Bucket.Name
+		key := s3record.S3.Object.Key
+
+		s3GetObjectInput := &s3.GetObjectInput{
+			Bucket: &bucket,
+			Key:    &key,
+		}
+		s3File, err := s3Client.GetObject(s3GetObjectInput)
+		if err != nil {
+			return err
+		}
+
+		colors, err := utils.GetProminentColors(s3File.Body, palette)
+
+		if err != nil {
+			return err
+		}
+
+		newRecord := ImageRecord{
+			ID:     "s3://" + bucket + "/" + key,
+			Bucket: bucket,
+			Key:    key,
+			Colors: colors,
+		}
+
+		// index the record in elastic search
+		body, err := json.Marshal(newRecord)
+		if err != nil {
+			return err
+		}
+
+		req := esapi.IndexRequest{
+			Index:      esIndex,
+			DocumentID: newRecord.ID,
+			Body:       bytes.NewReader(body),
+			Refresh:    "true",
+		}
+
+		// Perform the request with the client.
+		res, err := req.Do(context.Background(), esClient)
+		if err != nil {
+			return err
+		}
+		defer res.Body.Close()
+
+		if res.IsError() {
+			return fmt.Errorf("[%s] Error indexing document ID=%s", res.Status(), newRecord.ID)
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,14 +1,20 @@
 package utils
 
 import (
+	"github.com/EdlinOrg/prominentcolor"
 	chromath "github.com/jkl1337/go-chromath"
 	"github.com/jkl1337/go-chromath/deltae"
 	"math"
+
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
+	"io"
 )
 
 type Palette map[string][]uint32
 
-func Rgb2lab(r, g, b uint32) chromath.Lab {
+func rgb2lab(r, g, b uint32) chromath.Lab {
 	src := chromath.RGB{float64(r), float64(g), float64(b)}
 
 	targetIlluminant := &chromath.IlluminantRefD50
@@ -21,12 +27,12 @@ func Rgb2lab(r, g, b uint32) chromath.Lab {
 	return colorLab
 }
 
-func GetClosestColor(r, g, b uint32, p Palette) string {
+func getClosestColorName(color prominentcolor.ColorRGB, p Palette) string {
 	minDiff := math.MaxFloat64
 	minColor := ""
-	colorLab := Rgb2lab(r, g, b)
+	colorLab := rgb2lab(color.R, color.G, color.B)
 	for colorName, color := range p {
-		currLab := Rgb2lab(color[0], color[1], color[2])
+		currLab := rgb2lab(color[0], color[1], color[2])
 		currDiff := deltae.CIE2000(colorLab, currLab, &deltae.KLChDefault)
 
 		if currDiff < minDiff {
@@ -36,4 +42,33 @@ func GetClosestColor(r, g, b uint32, p Palette) string {
 	}
 
 	return minColor
+}
+
+func appendIfMissing(slice []string, value string) []string {
+	for _, ele := range slice {
+		if ele == value {
+			return slice
+		}
+	}
+	return append(slice, value)
+}
+
+func GetProminentColors(imageContent io.Reader, palette Palette) ([]string, error) {
+	img, _, err := image.Decode(imageContent)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := prominentcolor.Kmeans(img)
+	if err != nil {
+		return nil, err
+	}
+
+	colors := []string{}
+
+	for _, match := range res {
+		colors = appendIfMissing(colors, getClosestColorName(match.Color, palette))
+	}
+
+	return colors, nil
 }
