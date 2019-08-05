2046c3e3378c9cb67ceadc408215ee22a2ab5304
Luciano Mammino <lucianomammino@gmail.com>
2019-08-05 12:57:28 +0100

    Update run.sh
    Added moar comments

--- tests/integration/run.sh
+++ tests/integration/run.sh
@@ -3,13 +3,21 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $DIR
 
+# starts the services (test-runner will do nothing here)
 docker-compose up -d --build
+
+# we manually run the test runner with its own start script
+# this command will block until the test runner exits
 docker-compose run test-runner /opt/scripts/start.sh
 
+# we capture the return code of the test runner
 rc=$?
-echo -e "Completed with Exit code: ${rc}"
 
+# print result and clean up
+echo -e "Completed with Exit code: ${rc}"
 echo -e "Cleaning up docker..."
 docker-compose down
 
+# we exit with the same exit code as the test runner
+# If succeeded the test will be marked as succeeded, otherwise it will be marked as failed
 exit $rc
